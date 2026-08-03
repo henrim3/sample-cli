cc450bf1e33835a8ee2208f0d8297301f96ab6ef
Matti Henriksen <hen.ittam@gmail.com>
2026-08-03 03:37:20 -0400

    Remove empty file

--- src/Action.cpp
+++
@@ -1,6 +0,0 @@
-#include "Action.h"
-
-// Action::Action( const InitializeActionArgs & args )
-//     : command_type( args.command_type ), is_global( args.is_global ),
-//       args( args.args ) {}
-//
